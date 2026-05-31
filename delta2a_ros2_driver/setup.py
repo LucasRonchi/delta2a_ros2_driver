bc2311fc49dd751f5180fe2b1833f5272bd2640b
Lucas Ronchi <lucascronchi2005@gmail.com>
2026-05-31 18:48:56 -0300

    Create pkg

---
+++ delta2a_ros2_driver/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'delta2a_ros2_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='LucasRonchi',
+    maintainer_email='lucascronchi2005@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            f'lidar_pub = {package_name}.lidar_node:main'
+        ],
+    },
+)
